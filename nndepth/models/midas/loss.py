fix normalization depth

nndepth/models/midas/loss.py
@@ -190,26 +190,25 @@ class MiDasLoss(nn.Module):
             Tuple[torch.Tensor, dict]: loss and metrics
         """
         # Compute SSI trimmed loss
-        gt_scale, gt_shift = scale_shift_estimation(target, valid_mask)
-        ssi_target = ssi_depth(target, gt_scale, gt_shift)
-        normalized_target = normalize_01_depth(ssi_target, valid_mask)
+        normalized_target = normalize_01_depth(target, valid_mask)
+        gt_scale, gt_shift = scale_shift_estimation(normalized_target, valid_mask)
+        ssi_target = ssi_depth(normalized_target, gt_scale, gt_shift)
 
         pred_scale, pred_shift = scale_shift_estimation(pred, valid_mask)
         ssi_pred = ssi_depth(pred, pred_scale, pred_shift)
-        normalized_pred = normalize_01_depth(ssi_pred, valid_mask)
 
         # Compute SSI trimmed loss
-        mae = mae_loss(normalized_pred, normalized_target, valid_mask)
+        mae = mae_loss(ssi_pred, ssi_target, valid_mask)
         trimed_mae = trim_loss(mae)
         trimed_mae = average_loss(trimed_mae)
 
         # Compute SSI gradient matching loss
-        gradient_reg = gradient_regulizer(normalized_target, normalized_pred, valid_mask)
+        gradient_reg = gradient_regulizer(normalized_target, ssi_pred, valid_mask)
 
         loss = trimed_mae * self.trimmed_loss_coef + gradient_reg * self.gradient_reg_coef
 
         # Absolute deviation
-        abs_dev = torch.abs(normalized_pred - normalized_target)
+        abs_dev = torch.abs(ssi_pred - ssi_target)
         abs_dev = abs_dev[valid_mask]
         abs_dev = abs_dev.mean()
 

nndepth/models/midas/trainer.py
@@ -139,13 +139,14 @@ class MiDasTrainer(BaseTrainer):
         pred_depth_view = Depth(pred_depth[0], valid_mask[0]).get_view(cmap="magma")
 
         # Error map
-        gt_scale, gt_shift = scale_shift_estimation(gt, valid_mask)
-        gt_ssi = ssi_depth(gt, gt_scale, gt_shift)
-        gt_normalized = normalize_01_depth(gt_ssi, valid_mask)
-        pred_ssi = ssi_depth(pred_depth, gt_scale, gt_shift)
-        pred_normalized = normalize_01_depth(pred_ssi, valid_mask)
+        gt_normalized = normalize_01_depth(gt, valid_mask)
+        gt_scale, gt_shift = scale_shift_estimation(gt_normalized, valid_mask)
+        gt_ssi = ssi_depth(gt_normalized, gt_scale, gt_shift)
 
-        abs_dev = torch.abs(pred_normalized - gt_normalized)
+        pred_scale, pred_shift = scale_shift_estimation(pred_depth, valid_mask)
+        pred_ssi = ssi_depth(pred_depth, pred_scale, pred_shift)
+
+        abs_dev = torch.abs(pred_ssi - gt_ssi)
         min_abs_dev = abs_dev[valid_mask].min()
         max_abs_dev = abs_dev[valid_mask].max()
         abs_dev = (abs_dev - min_abs_dev) / (max_abs_dev - min_abs_dev)
@@ -328,6 +329,7 @@ class MiDasTrainer(BaseTrainer):
         Returns:
             dict: metrics of the evaluation
         """
+        logger.info("Evaluating...")
         val_metrics = {}
         for i_batch, batch in enumerate(tqdm(dataloader, desc="Evaluating")):
             if self.num_val_samples is not None and i_batch >= self.num_val_samples: